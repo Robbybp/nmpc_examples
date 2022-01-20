d27cbb5ff10bb3cf2d824f1034b3ce2eae2b8926
Robert Parker <robbybparker@gmail.com>
2022-01-20 17:48:07 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="nmpc_prototype",
+    description=(
+        "Prototyping data structures for rolling-horizon dynamic "
+        "optimization."
+        ),
+    author="Robert Parker",
+    author_email="rparker1@andrew.cmu.edu",
+    license="BSD-3",
+    packages=["nmpc_prototype"],
+)

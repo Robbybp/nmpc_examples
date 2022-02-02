0976f745d452b032d0ec330d3e6bcd7796132a48
Robert <robbybparker@gmail.com>
2022-02-01 22:45:12 -0500

    use CUIDs instead of names as keys

--- nmpc_examples/nmpc/model_helper.py
+++ nmpc_examples/nmpc/model_helper.py
@@ -4,6 +4,13 @@ from pyomo.core.base.componentuid import ComponentUID
 
 
 class DynamicModelHelper(object):
+    """
+    Somewhat like a serializer, with the ability to generate and load
+    different types of data from a dynamic model. Also somewhat like a
+    wrapper around the flattened indexed variables, with the ability
+    to shift values between different points in time.
+
+    """
 
     def __init__(self, model, time):
         scalar_vars, dae_vars = flatten_dae_components(model, time, Var)
@@ -11,36 +18,40 @@ class DynamicModelHelper(object):
         self.time = time
         self.scalar_vars = scalar_vars
         self.dae_vars = dae_vars
+
+        # Use buffer to reduce repeated work during name/cuid generation
         cuid_buffer = {}
-        self.scalar_var_names = [
-            str(ComponentUID(var, cuid_buffer=cuid_buffer))
+        self.scalar_var_cuids = [
+            ComponentUID(var, cuid_buffer=cuid_buffer)
             for var in self.scalar_vars
         ]
-        self.dae_var_names = [
-            str(ComponentUID(var.referent, cuid_buffer=cuid_buffer))
+        self.dae_var_cuids = [
+            ComponentUID(var.referent, cuid_buffer=cuid_buffer)
             for var in self.dae_vars
         ]
-        self.scalar_name_var_map = dict(
-            zip(self.scalar_var_names, self.scalar_vars)
-        )
-        self.dae_name_var_map = dict(
-            zip(self.dae_var_names, self.dae_vars)
-        )
 
     def get_scalar_data(self):
         return {
-            name: var.value
-            for name, var in zip(self.scalar_var_names, self.scalar_vars)
+            cuid: var.value
+            for cuid, var in zip(self.scalar_var_cuids, self.scalar_vars)
         }
 
     def get_data_at_time(self, t0=None):
         if t0 is None:
             t0 = self.time.first()
         return {
-            name: var[t0].value
-            for name, var in zip(self.dae_var_names, self.dae_vars)
+            cuid: var[t0].value
+            for cuid, var in zip(self.dae_var_cuids, self.dae_vars)
         }
 
     def load_scalar_data(self, data):
-        for name, val in data.items():
-            self.scalar_name_var_map[name].set_value(val)
+        # Unclear whether methods or loading data into a model should
+        # go on this class.
+        #
+        # Also unclear whether mapping names to variables is actually
+        # a good idea. If keys in data are just CUIDs, then computing
+        # the hash (to look up a variable) is about the same amount
+        # of work as calling find_component.
+        for cuid, val in data.items():
+            var = self.model.find_component(cuid)
+            var.set_value(val)

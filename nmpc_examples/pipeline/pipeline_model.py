7352c4a796cc2440d2eeec2d1a4b04ff02a614f9
Robert Parker <robbybparker@gmail.com>
2022-04-06 15:56:08 -0400

    update imports

--- nmpc_examples/pipeline/pipeline_model.py
+++ nmpc_examples/pipeline/pipeline_model.py
@@ -1,9 +1,9 @@
 import pyomo.environ as pyo
 import idaes.core as idaes
-from idaes.gas_distribution.properties.natural_gas import (
+from idaes.models_extra.gas_distribution.properties.natural_gas import (
     NaturalGasParameterBlock,
 )
-from idaes.gas_distribution.unit_models.pipeline import GasPipeline
+from idaes.models_extra.gas_distribution.unit_models.pipeline import GasPipeline
 
 from nmpc_examples.nmpc.dynamic_data.series_data import (
     TimeSeriesData,
